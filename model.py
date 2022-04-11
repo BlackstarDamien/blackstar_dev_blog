06e3f2477f55a8fde851ace741302fbc2998e1d4
BlackstarDamien <damianzylka@egmail.com>
2022-04-11 18:12:24 +0200

    #1 Simplify Article entity

--- model.py
+++ model.py
@@ -5,29 +5,16 @@ from typing import List
 class Article:
     def __init__(
         self,
+        title: str,
         author: str,
         publication_date: datetime,
         tags: List[str],
         description: str,
         content: str,
     ):
-        self.__author = author
-        self.__publication_date = publication_date
-        self.__tags = tags
-        self.__description = description
-        self.__content = content
-
-    def who_wrote_it(self) -> str:
-        return self.__author
-
-    def when_was_published(self) -> datetime:
-        return self.__publication_date
-
-    def which_tags_it_has(self) -> List[str]:
-        return self.__tags
-
-    def what_is_about(self) -> str:
-        return self.__description
-
-    def read(self) -> str:
-        return self.__content
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.tags = tags
+        self.description = description
+        self.content = content
