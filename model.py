2f2e6a81088b206e72f733000b19e63d3a58a3f0
BlackstarDamien <damianzylka@egmail.com>
2022-04-25 23:14:40 +0200

    #2 Create Tag Value Object with necessary exceptions

--- model.py
+++ model.py
@@ -2,13 +2,40 @@ from datetime import datetime
 from typing import List
 
 
+class TagNameTooLong(Exception):
+    pass
+
+
+class EmptyTagName(Exception):
+    pass
+
+
+class Tag:
+    def __init__(self, name: str):
+        self.name = name
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) == 0:
+            raise EmptyTagName("Tag name should not be empty.")
+
+        if len(value) > 20:
+            raise TagNameTooLong("Provided name exceeded max length of 20 characters.")
+
+        self.name = value
+
+
 class Article:
     def __init__(
         self,
         title: str,
         author: str,
         publication_date: datetime,
-        tags: List[str],
+        tags: List[Tag],
         description: str,
         content: str,
     ):
