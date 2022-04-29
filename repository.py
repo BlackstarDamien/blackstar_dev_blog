c43b6b911b3251fc63361e7656aebf5dc12fa34b
BlackstarDamien <damianzylka@egmail.com>
2022-04-29 15:57:34 +0200

    #2 Fix typo in script name

---
+++ repository.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod, ABC
+from model import Article
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    def add(self, article: Article):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, title: str):
+        raise NotImplementedError
+
+
+class SQLAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, article: Article):
+        self.session.add(article)
+
+    def get(self, title: str) -> Article:
+        result = self.session.query(Article).filter_by(title=title).one()
+        return result
