b35b75b4c3215a1bb8fdeff3bcc38db2b6e88598
BlackstarDamien <damianzylka@egmail.com>
2022-05-13 21:16:08 +0000

    #6 Add get_postgres_uri function to config

--- config.py
+++ config.py
@@ -6,3 +6,13 @@ def get_api_url():
     port = 5005 if host == "localhost" else 80
     url = f"http://{host}:{port}"
     return url
+
+
+def get_postgres_uri():
+    host = os.getenv("DB_HOST", "localhost")
+    port = 54321 if host == "localhost" else 5432
+    password = os.getenv("DB_PASSWORD", "test123")
+    user = "blog_user"
+    db_name = "articles"
+    uri = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+    return uri
