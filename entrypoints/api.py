a55b5a8b77557a83b8e9933cae9b92bdd61daec0
BlackstarDamien <damianzylka@egmail.com>
2022-05-13 21:15:28 +0000

    #6 Add another endpoint

--- entrypoints/api.py
+++ entrypoints/api.py
@@ -1,9 +1,23 @@
+import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from flask import Flask
 
+from adapters import orm, repository
 
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
 @app.route("/health")
 def health_check():
     return "<h1>Hoooray! We are online!</h1>"
+
+@app.route("/articles")
+def get_articles():
+    session = get_session()
+    repo = repostiory.SQLAlchemyRepository(session)
+    articles = repo.list_items()
+
+    return articles, 200
