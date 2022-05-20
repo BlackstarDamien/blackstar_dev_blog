7663621b207ee586d595c275907b94f19579ca40
damian.zylka <damianzylka@gmail.com>
2022-05-20 18:26:45 +0000

    #6 Add edit_article() into services

--- service_layer/services.py
+++ service_layer/services.py
@@ -2,6 +2,7 @@ from adapters.repository import AbstractRepository
 from domain.model import Article
 from typing import List
 from .exceptions import ArticleNotFound, ArticleAlreadyExists
+from copy import deepcopy
 
 def list_articles(repository: AbstractRepository) -> List[Article]:
     return repository.list_items()
@@ -23,4 +24,16 @@ def add_article(new_article: Article, repository: AbstractRepository, session):
 
 def remove_article(title: str, repository: AbstractRepository, session):
     repository.remove(title)
+    session.commit()
+
+def edit_article(title: str, data: dict, repository: AbstractRepository, session):
+    article = repository.get(title)
+    new_article = deepcopy(article)
+
+    for field in data:
+        article_field = getattr(new_article, field)
+        setattr(new_article, field, data[field])
+
+    remove_article(article.title, repository, session)
+    add_article(new_article, repository, session)
     session.commit()
