8f5d7b79ccd96186b13d39ec33ebc1b1035252a6
damian.zylka <damianzylka@gmail.com>
2022-05-20 17:23:08 +0000

    #6 Extend add_article() in services module
    - Add handling case when added article already exists

--- service_layer/services.py
+++ service_layer/services.py
@@ -1,7 +1,7 @@
 from adapters.repository import AbstractRepository
 from domain.model import Article
 from typing import List
-from .exceptions import ArticleNotFound
+from .exceptions import ArticleNotFound, ArticleAlreadyExists
 
 def list_articles(repository: AbstractRepository) -> List[Article]:
     return repository.list_items()
@@ -14,5 +14,9 @@ def get_article(title: str, repository: AbstractRepository) -> Article:
     return article
 
 def add_article(new_article: Article, repository: AbstractRepository, session):
+    articles = list_articles(repository)
+    if new_article in articles:
+        raise ArticleAlreadyExists(f"Article with title '{new_article.title}' already exists.")
+
     repository.add(new_article)
     session.commit()
