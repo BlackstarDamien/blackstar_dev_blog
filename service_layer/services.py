a56be9091218691369e52b499da1715529fc2efc
damian.zylka <damianzylka@gmail.com>
2022-05-20 16:47:10 +0000

    #6 Add get_article() into services

--- service_layer/services.py
+++ service_layer/services.py
@@ -4,3 +4,6 @@ from typing import List
 
 def list_articles(repository: AbstractRepository) -> List[Article]:
     return repository.list_items()
+
+def get_article(title: str, repository: AbstractRepository) -> Article:
+    return repository.get(title)
