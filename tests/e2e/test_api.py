f8bfd31ebdd236a2da29f8e0f21392110acd13f2
damian.zylka <damianzylka@gmail.com>
2022-05-26 18:44:27 +0000

    #8 Reorganize e2e tests

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,17 +1,30 @@
 from datetime import date
 from config import get_api_url
 import requests
+from typing import Optional, List
+import pytest
 
 
-def test_api_returns_all_articles(add_articles, add_tags_to_article):
-    add_articles([
-        ("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum..."),
-        ("Why Docker is the best", "Carl Johnson", "2016-05-09", "Why Docker is cool", "Docker is cool"),
-        ("PHP - A language of the internet", "John Doe", "2018-10-12", "About PHP", "Lorem ipsum..")
-    ])
-    add_tags_to_article(['Docker', 'Infrastructure'],
-                        title="Why Docker is the best",
-                        author="Carl Johnson",)
+def post_to_add_article(title: str, author: str, publication_date: date,
+                        description:str, content: str, tags: Optional[List[str]] = []):
+    url = get_api_url()
+    article_to_add = {
+        "title": title,
+        "author": author,
+        "publication_date": publication_date,
+        "description": description,
+        "content": content,
+        "tags": tags
+    }
+    response = requests.post(f"{url}/article", json=article_to_add)
+
+    assert response.status_code == 201
+
+@pytest.mark.usefixtures("postgres_session")
+def test_articles_endpoint_returns_200_and_all_articles():
+    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
+    post_to_add_article("Why Docker is the best", "Carl Johnson", "2016-05-09", "Why Docker is cool", "Docker is cool", ["Docker", "Infrastructure"])
+    post_to_add_article("PHP - A language of the internet", "John Doe", "2018-10-12", "About PHP", "Lorem ipsum..")
 
     api_url = get_api_url()
     articles = requests.get(f"{api_url}/articles")
@@ -19,11 +32,9 @@ def test_api_returns_all_articles(add_articles, add_tags_to_article):
     assert articles.status_code == 200
     assert len(articles.json()["articles"]) == 3
 
-
-def test_api_returns_specified_article(add_articles):
-    add_articles([
-        ("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
-    ])
+@pytest.mark.usefixtures("postgres_session")
+def test_article_endpoint_returns_200_and_specified_article():
+    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
     searched_title = "Async Libraries in Python"
     api_url = get_api_url()
     article = requests.get(f"{api_url}/article", json={"title": searched_title})
@@ -31,10 +42,24 @@ def test_api_returns_specified_article(add_articles):
     assert article.status_code == 200
     assert article.json()["title"] == searched_title
 
+@pytest.mark.usefixtures("postgres_session")
+def test_article_endpoint_returns_404_and_specified_article():
+    searched_title = "Article That Does Not Exist"
+    api_url = get_api_url()
+    article = requests.get(f"{api_url}/article", json={"title": searched_title})
 
-def test_api_should_save_articles():
+    assert article.status_code == 404
+    assert article.json()["message"] == f"Article with title '{searched_title}' not found."
+
+@pytest.mark.usefixtures("postgres_session")
+def test_article_should_return_400_and_error_message():
     with open("./tests/test_data/example_article.txt") as f:
         article_content = f.read()
+
+    post_to_add_article("Custom hooks in React",
+                        "Tommy Vercetti" , "2022-12-05",
+                        "Let's see how you can easily create your own hooks in React",
+                        article_content, ["Hooks", "React", "Javascript"])
 
     article_to_add = {
         "title": "Custom hooks in React",
@@ -48,17 +73,5 @@ def test_api_should_save_articles():
     api_url = get_api_url()
     post_request = requests.post(f"{api_url}/article", json=article_to_add)
 
-    assert post_request.status_code == 201
-
-    added_article = requests.get(
-        f"{api_url}/article", json={"title": article_to_add["title"]}
-    )
-    added_article_json = added_article.json()
-
-    assert added_article.status_code == 200
-    assert added_article_json["title"] == article_to_add["title"]
-    assert added_article_json["author"] == article_to_add["author"]
-    assert added_article_json["publication_date"] == article_to_add["publication_date"]
-    assert added_article_json["description"] == article_to_add["description"]
-    assert added_article_json["content"] == article_to_add["content"]
-    assert sorted(added_article_json["tags"]) == sorted(article_to_add["tags"])
+    assert post_request.status_code == 400
+    assert post_request.json()["message"] == f"Article with title '{article_to_add['title']}' already exists."
