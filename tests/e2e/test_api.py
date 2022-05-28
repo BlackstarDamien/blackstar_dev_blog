e0fa177d2dad747ae5cc0af1e3b98ff552b15d41
damian.zylka <damianzylka@gmail.com>
2022-05-28 12:59:47 +0000

    #8 Give e2e tests more meaningful names

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -21,7 +21,7 @@ def post_to_add_article(title: str, author: str, publication_date: date,
     assert response.status_code == 201
 
 @pytest.mark.usefixtures("postgres_session")
-def test_articles_endpoint_returns_200_and_all_articles():
+def test_get_articles_endpoint_returns_200_and_all_articles():
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
     post_to_add_article("Why Docker is the best", "Carl Johnson", "2016-05-09", "Why Docker is cool", "Docker is cool", ["Docker", "Infrastructure"])
     post_to_add_article("PHP - A language of the internet", "John Doe", "2018-10-12", "About PHP", "Lorem ipsum..")
@@ -33,7 +33,7 @@ def test_articles_endpoint_returns_200_and_all_articles():
     assert len(articles.json()["articles"]) == 3
 
 @pytest.mark.usefixtures("postgres_session")
-def test_article_endpoint_returns_200_and_specified_article():
+def test_get_article_endpoint_returns_200_and_specified_article():
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
     searched_title = "Async Libraries in Python"
     api_url = get_api_url()
@@ -43,7 +43,7 @@ def test_article_endpoint_returns_200_and_specified_article():
     assert article.json()["title"] == searched_title
 
 @pytest.mark.usefixtures("postgres_session")
-def test_article_endpoint_returns_404_and_specified_article():
+def test_get_article_endpoint_returns_404_and_specified_article():
     searched_title = "Article That Does Not Exist"
     api_url = get_api_url()
     article = requests.get(f"{api_url}/article", json={"title": searched_title})
@@ -52,7 +52,7 @@ def test_article_endpoint_returns_404_and_specified_article():
     assert article.json()["message"] == f"Article with title '{searched_title}' not found."
 
 @pytest.mark.usefixtures("postgres_session")
-def test_article_should_return_400_and_error_message():
+def test_post_article_should_return_400_and_error_message():
     with open("./tests/test_data/example_article.txt") as f:
         article_content = f.read()
 
