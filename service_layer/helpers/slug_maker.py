7bd97f7cfdabd0264febbfb2ae60ac3fe4b99f40
damian.zylka <damianzylka@gmail.com>
2022-06-07 21:37:04 +0000

    #14 Remove unnecessary slug_maker module with tests

--- service_layer/helpers/slug_maker.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from typing import Optional
-
-def create_slug(title: str, chars_limit: Optional[int]=None) -> str:
-    chars_limit = chars_limit if chars_limit else len(title)
-    slug_title = title.lower()
-    slug_title = slug_title.replace(" ", "-")
-    return f"{slug_title[:chars_limit]}"
