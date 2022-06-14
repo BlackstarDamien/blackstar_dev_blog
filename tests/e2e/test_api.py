2771e000cd8c99020a4c6b3fa57491017996e8c9
damian.zylka <damianzylka@gmail.com>
2022-06-14 15:29:39 +0000

    #4 Add docstrings into e2e tests

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -9,6 +9,24 @@ from config import get_api_url
 
 def post_to_add_article(title: str, author: str, publication_date: date,
                         description:str, content: str, tags: Optional[List[str]] = []):
+    """Sends POST request into API to create new article described
+    by provided arguments.
+
+    Parameters
+    ----------
+    title : str
+        Article's title.
+    author : str
+        Article's author.
+    publication_date : date
+        When an article was published.
+    description : str
+        Briefly described what article is about.
+    content : str
+        Actual content of an article/
+    tags : Optional[List[str]], optional
+        List of tags assigned to an article, by default []
+    """
     url = get_api_url()
     article_to_add = {
         "title": title,
@@ -24,6 +42,8 @@ def post_to_add_article(title: str, author: str, publication_date: date,
 
 @pytest.mark.usefixtures("postgres_session")
 def test_get_articles_endpoint_returns_200_and_all_articles():
+    """Tests that API is able to list all available articles.
+    """
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
     post_to_add_article("Why Docker is the best", "Carl Johnson", "2016-05-09", "Why Docker is cool", "Docker is cool", ["Docker", "Infrastructure"])
     post_to_add_article("PHP - A language of the internet", "John Doe", "2018-10-12", "About PHP", "Lorem ipsum..")
@@ -36,6 +56,8 @@ def test_get_articles_endpoint_returns_200_and_all_articles():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_get_article_endpoint_returns_200_and_specified_article():
+    """Tests that API is able to provide article for given title.
+    """
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
     searched_title = "Async Libraries in Python"
     api_url = get_api_url()
@@ -46,6 +68,9 @@ def test_get_article_endpoint_returns_200_and_specified_article():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_get_article_endpoint_returns_404_and_specified_article():
+    """Tests that API is able to send error message when article
+    is not found.
+    """
     api_url = get_api_url()
     article = requests.get(f"{api_url}/articles/article-that-does-not-exist")
 
@@ -54,6 +79,9 @@ def test_get_article_endpoint_returns_404_and_specified_article():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_post_article_should_return_400_and_error_message():
+    """Tests that API returns error message when article meant to create
+    already exists.
+    """
     with open("./tests/test_data/example_article.txt") as f:
         article_content = f.read()
 
@@ -79,6 +107,9 @@ def test_post_article_should_return_400_and_error_message():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_patch_article_should_return_200_and_success_message():
+    """Tests that API is able to edit existing article with new values
+    of it's own properties.
+    """
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...", ["Async", "Programming"])
     fields_to_change = {
         "content": "Some new fresh content",
@@ -94,6 +125,9 @@ def test_patch_article_should_return_200_and_success_message():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_patch_articles_should_return_400_and_error_message():
+    """Tests that API returns error message when trying to
+    edit nonexistent article.
+    """
     fields_to_change = {
         "content": "Some new fresh content",
         "description": "Some new description",
@@ -108,6 +142,8 @@ def test_patch_articles_should_return_400_and_error_message():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_delete_article_should_return_200_and_success_message():
+    """Tests that API can remove existig article.
+    """
     post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
 
     api_url = get_api_url()
@@ -118,6 +154,9 @@ def test_delete_article_should_return_200_and_success_message():
 
 @pytest.mark.usefixtures("postgres_session")
 def test_delete_article_should_return_400_and_error_message():
+    """Tests that API throws error message when article
+    meant to remove does not exist.
+    """
     api_url = get_api_url()
     delete_request = requests.delete(f"{api_url}/articles/async-libraries-in-python")
 
