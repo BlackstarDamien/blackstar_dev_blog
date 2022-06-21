8dec65febee24dc273f2289d4775c7a10ee4a403
BlackstarDamien <damianzylka@egmail.com>
2022-06-21 20:01:00 +0200

    #24 Apply code formating

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -7,8 +7,14 @@ import requests
 from config import get_api_url
 
 
-def post_to_add_article(title: str, author: str, publication_date: date,
-                        description:str, content: str, tags: Optional[List[str]] = []):
+def post_to_add_article(
+    title: str,
+    author: str,
+    publication_date: date,
+    description: str,
+    content: str,
+    tags: Optional[List[str]] = [],
+):
     """Sends POST request into API to create new article described
     by provided arguments.
 
@@ -34,19 +40,38 @@ def post_to_add_article(title: str, author: str, publication_date: date,
         "publication_date": publication_date,
         "description": description,
         "content": content,
-        "tags": tags
+        "tags": tags,
     }
     response = requests.post(f"{url}/articles", json=article_to_add)
 
     assert response.status_code == 201
 
+
 @pytest.mark.usefixtures("postgres_session")
 def test_get_articles_endpoint_returns_200_and_all_articles():
-    """Tests that API is able to list all available articles.
-    """
-    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
-    post_to_add_article("Why Docker is the best", "Carl Johnson", "2016-05-09", "Why Docker is cool", "Docker is cool", ["Docker", "Infrastructure"])
-    post_to_add_article("PHP - A language of the internet", "John Doe", "2018-10-12", "About PHP", "Lorem ipsum..")
+    """Tests that API is able to list all available articles."""
+    post_to_add_article(
+        "Async Libraries in Python",
+        "Tom Smith",
+        "2022-01-01",
+        "Some async libs",
+        "Lorem ipsum...",
+    )
+    post_to_add_article(
+        "Why Docker is the best",
+        "Carl Johnson",
+        "2016-05-09",
+        "Why Docker is cool",
+        "Docker is cool",
+        ["Docker", "Infrastructure"],
+    )
+    post_to_add_article(
+        "PHP - A language of the internet",
+        "John Doe",
+        "2018-10-12",
+        "About PHP",
+        "Lorem ipsum..",
+    )
 
     api_url = get_api_url()
     articles = requests.get(f"{api_url}/articles")
@@ -54,17 +79,24 @@ def test_get_articles_endpoint_returns_200_and_all_articles():
     assert articles.status_code == 200
     assert len(articles.json()["articles"]) == 3
 
+
 @pytest.mark.usefixtures("postgres_session")
 def test_get_article_endpoint_returns_200_and_specified_article():
-    """Tests that API is able to provide article for given title.
-    """
-    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
+    """Tests that API is able to provide article for given title."""
+    post_to_add_article(
+        "Async Libraries in Python",
+        "Tom Smith",
+        "2022-01-01",
+        "Some async libs",
+        "Lorem ipsum...",
+    )
     searched_title = "Async Libraries in Python"
     api_url = get_api_url()
     article = requests.get(f"{api_url}/articles/async-libraries-in-python")
 
     assert article.status_code == 200
     assert article.json()["title"] == searched_title
+
 
 @pytest.mark.usefixtures("postgres_session")
 def test_get_article_endpoint_returns_404_and_specified_article():
@@ -77,6 +109,7 @@ def test_get_article_endpoint_returns_404_and_specified_article():
     assert article.status_code == 404
     assert article.json()["message"] == f"Article not found."
 
+
 @pytest.mark.usefixtures("postgres_session")
 def test_post_article_should_return_400_and_error_message():
     """Tests that API returns error message when article meant to create
@@ -85,10 +118,14 @@ def test_post_article_should_return_400_and_error_message():
     with open("./tests/test_data/example_article.txt") as f:
         article_content = f.read()
 
-    post_to_add_article("Custom hooks in React",
-                        "Tommy Vercetti" , "2022-12-05",
-                        "Let's see how you can easily create your own hooks in React",
-                        article_content, ["Hooks", "React", "Javascript"])
+    post_to_add_article(
+        "Custom hooks in React",
+        "Tommy Vercetti",
+        "2022-12-05",
+        "Let's see how you can easily create your own hooks in React",
+        article_content,
+        ["Hooks", "React", "Javascript"],
+    )
 
     article_to_add = {
         "title": "Custom hooks in React",
@@ -105,23 +142,34 @@ def test_post_article_should_return_400_and_error_message():
     assert post_request.status_code == 400
     assert post_request.json()["message"] == f"Article already exists."
 
+
 @pytest.mark.usefixtures("postgres_session")
 def test_patch_article_should_return_200_and_success_message():
     """Tests that API is able to edit existing article with new values
     of it's own properties.
     """
-    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...", ["Async", "Programming"])
+    post_to_add_article(
+        "Async Libraries in Python",
+        "Tom Smith",
+        "2022-01-01",
+        "Some async libs",
+        "Lorem ipsum...",
+        ["Async", "Programming"],
+    )
     fields_to_change = {
         "content": "Some new fresh content",
         "description": "Some new description",
-        "tags": ["Python", "Coding"]
+        "tags": ["Python", "Coding"],
     }
 
     api_url = get_api_url()
-    patch_request = requests.patch(f"{api_url}/articles/async-libraries-in-python", json=fields_to_change)
+    patch_request = requests.patch(
+        f"{api_url}/articles/async-libraries-in-python", json=fields_to_change
+    )
 
     assert patch_request.status_code == 200
     assert patch_request.json()["message"] == "Article successfully edited."
+
 
 @pytest.mark.usefixtures("postgres_session")
 def test_patch_articles_should_return_400_and_error_message():
@@ -131,26 +179,35 @@ def test_patch_articles_should_return_400_and_error_message():
     fields_to_change = {
         "content": "Some new fresh content",
         "description": "Some new description",
-        "tags": ["Python", "Coding"]
+        "tags": ["Python", "Coding"],
     }
 
     api_url = get_api_url()
-    patch_request = requests.patch(f"{api_url}/articles/async-libraries-in-python", json=fields_to_change)
+    patch_request = requests.patch(
+        f"{api_url}/articles/async-libraries-in-python", json=fields_to_change
+    )
 
     assert patch_request.status_code == 404
     assert patch_request.json()["message"] == "Article not found."
 
+
 @pytest.mark.usefixtures("postgres_session")
 def test_delete_article_should_return_200_and_success_message():
-    """Tests that API can remove existig article.
-    """
-    post_to_add_article("Async Libraries in Python", "Tom Smith", "2022-01-01", "Some async libs", "Lorem ipsum...")
+    """Tests that API can remove existig article."""
+    post_to_add_article(
+        "Async Libraries in Python",
+        "Tom Smith",
+        "2022-01-01",
+        "Some async libs",
+        "Lorem ipsum...",
+    )
 
     api_url = get_api_url()
     delete_request = requests.delete(f"{api_url}/articles/async-libraries-in-python")
 
     assert delete_request.status_code == 200
     assert delete_request.json()["message"] == "Article successfully removed."
+
 
 @pytest.mark.usefixtures("postgres_session")
 def test_delete_article_should_return_400_and_error_message():
