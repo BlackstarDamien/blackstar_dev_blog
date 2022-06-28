b36d3de69fb32aec3571434a894e47c6cf5efed4
damian.zylka <damianzylka@gmail.com>
2022-06-28 17:36:06 +0000

    #26 Add another integration tests for unit of work

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,6 +1,24 @@
 from copy import deepcopy
 
+import pytest
 from blog_service.service_layer import unit_of_work
+
+TEST_DATA = {
+    "ref": "test-article",
+    "title": "Test Article",
+    "author": "Kukulek",
+    "date": "2022-01-01",
+    "desc": "Some cool article",
+    "content": "Something Something",
+}
+
+
+def insert_article(session, data_to_insert: dict):
+    session.execute(
+        "INSERT INTO articles(reference, title, author, publication_date, description, content) VALUES "
+        "(:ref, :title, :author, :date, :desc, :content);",
+        data_to_insert,
+    )
 
 
 def test_uow_can_fetch_article(session_factory):
@@ -8,15 +26,7 @@ def test_uow_can_fetch_article(session_factory):
     given reference.
     """
     session = session_factory()
-    insert_article(
-        session,
-        "test-article",
-        "Test Article",
-        "Kukulek",
-        "2022-01-01",
-        "Some cool article",
-        "Something Something",
-    )
+    insert_article(session, TEST_DATA)
     session.commit()
 
     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
@@ -26,13 +36,31 @@ def test_uow_can_fetch_article(session_factory):
     assert article.reference == "test-article"
 
 
-def insert_article(
-    session, ref: str, title: str, author: str, date: str, desc: str, content: str
-):
-    session.execute(
-        "INSERT INTO articles(reference, title, author, publication_date, description, content) VALUES "
-        "(:ref, :title, :author, :date, :desc, :content);",
-        dict(
-            ref=ref, title=title, author=author, date=date, desc=desc, content=content
-        ),
-    )
+def test_rolls_back_uncommitted_work_by_default(session_factory):
+    """Tests that unit of work reverts uncomitted changes in the session."""
+    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+
+    with uow:
+        insert_article(uow.session, TEST_DATA)
+    new_session = session_factory()
+    rows = list(new_session.execute('SELECT * FROM "articles"'))
+    assert rows == []
+
+
+def test_rolls_back_on_error(session_factory):
+    """Tests that unit of work is able to revert changes
+    when exception was thrown.
+    """
+
+    class TestException(Exception):
+        pass
+
+    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    with pytest.raises(TestException):
+        with uow:
+            insert_article(uow.session, TEST_DATA)
+        raise TestException
+
+    new_session = session_factory()
+    rows = list(new_session.execute('SELECT * FROM "articles"'))
+    assert rows == []
